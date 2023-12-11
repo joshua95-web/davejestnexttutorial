e3d325ba41cf64e2bca85c12443e2480713a802d
Joshua <122205306+joshua95-web@users.noreply.github.com>
2023-12-11 15:55:09 +0000

    tried to get simple calculator test working to no avail

--- __tests__/Calculator.test.tsx
+++ __tests__/Calculator.test.tsx
@@ -1,5 +1,5 @@
 import Calculator from "@/app/Calculator/page";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 describe("Calculator", () => {
   it("should render a calculator and its buttons", () => {
@@ -13,5 +13,16 @@ describe("Calculator", () => {
     expect(subtractButton).toBeInTheDocument();
     expect(multiplyButton).toBeInTheDocument();
     expect(divideButton).toBeInTheDocument();
+  });
+  it("adds numbers", () => {
+    render(<Calculator />);
+    const num1input = screen.getByTestId("num1");
+    const num2input = screen.getByTestId("num2");
+    const addButton = screen.getByTestId("add");
+    const resultArea = screen.getByTestId("result");
+    fireEvent.change(num1input, { target: { value: "5" } });
+    fireEvent.change(num2input, { target: { value: "8" } });
+    addButton.click();
+    expect(resultArea).toHaveTextContent("13");
   });
 });
